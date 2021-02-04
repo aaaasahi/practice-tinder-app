0253e95d9e3d54a151aba59c0cd469c41c28bcf8
asahi <asap1017@docomo.ne.jp>
2021-02-04 18:24:33 +0900

    initial~devise

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,9 @@
+class ApplicationController < ActionController::Base
+    before_action :configure_permitted_parameters, if: :devise_controller? #devise_controller使用時before実行
+
+    protected
+
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up,keys:[:name,:gender]) #sign_up時にkeyを許可
+    end
+end
