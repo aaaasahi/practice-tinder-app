e4d42a6f7e81ab2684928269c42c722b0cc18cfe
asahi <asap1017@docomo.ne.jp>
2021-02-05 10:17:11 +0900

    プロフィール編集機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,8 @@ class ApplicationController < ActionController::Base
 
     def configure_permitted_parameters
         devise_parameter_sanitizer.permit(:sign_up,keys:[:name,:gender]) #sign_up時にkeyを許可
+    
+        devise_parameter_sanitizer.permit(:account_update, keys:[:name,:self_introduction])
     end
+
 end
