16ed9478eb5e1e44cd7117f000d7c8902ff3aba2
asahi <asap1017@docomo.ne.jp>
2021-02-07 19:09:37 +0900

    スワイプページの設定

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,11 +2,15 @@ class RegistrationsController < Devise::RegistrationsController
 
     protected
   
-    def update_resource(resource, params)
+    def update_resource(resource, params) #パスワードなしでプロフィール変更できる
       resource.update_without_current_password(params)
     end
 
-    def after_update_path_for(resource) #更新後の遷移先をプロフィールページに変更できるメソッド
+    def after_update_path_for(resource) #プロフィール更新後の
         user_path(resource)
+      end
+
+      def after_sign_up_path_for(resource) #ログインしたら
+        users_path
       end
   end
